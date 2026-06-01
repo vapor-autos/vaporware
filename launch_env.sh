0769a681ceb25816eaf09f2ef55e4a68c04c3f99
souljasim <souljasim@users.noreply.github.com>
2026-05-31 23:49:28 -0400

    Load local launch env file

--- launch_env.sh
+++ launch_env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+LAUNCH_ENV_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
@@ -14,6 +16,12 @@ export VECLIB_MAXIMUM_THREADS=1
 # it to preempt the model workloads. we have enough
 # headroom for this until ui is moved to the CPU.
 export QCOM_PRIORITY=12
+
+if [ -f "$LAUNCH_ENV_DIR/.env" ]; then
+  set -a
+  source "$LAUNCH_ENV_DIR/.env"
+  set +a
+fi
 
 if [ -z "$AGNOS_VERSION" ]; then
   export AGNOS_VERSION="18.3"
